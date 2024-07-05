9e5e206f2a5241f5ed22e04739859c9aa6c0cdd8
ElisabettaMirti <belmirti@gmail.com>
2024-07-05 17:00:04 +0200

    aggiunto metodo per chiamare la funzione

--- js/script.js
+++ js/script.js
@@ -3,7 +3,26 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-            
-        }
+            toDoList: [],
+            apiURL: './api/get_all_tasks.php'
+            }
+        },
+        methods: {
+            getToDoList(){
+                axios.get(this.apiURL)
+                    .then((response) => {
+                    console.log(response);
+                    this.toDoList = response.data.tasks;
+                })
+                .catch(function (error) {
+                console.log(error);
+                })
+                .finally(function () {
+                // always executed
+                });  
+            }
+        },
+        created(){
+            this.getToDoList();
         }
     }).mount('#app')
